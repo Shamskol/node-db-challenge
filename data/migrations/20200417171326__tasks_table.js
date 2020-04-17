ecbb514496c5beed86a5f083a8b13b8f3792d134
Shamskol <koladejun@yahoo.com>
2020-04-17 17:41:15 +0100

    Created the migration tables

---
+++ data/migrations/20200417171326__tasks_table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable(`tasks`, tbl => {
+        tbl.increments();
+        tbl.string(`description`, 255).notNullable();
+        tbl.string(`notes`, 255);
+        tbl
+          .boolean(`completed`)
+          .notNullable()
+          .defaultTo(false);
+      });
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists(`tasks`);
+  
+};
